6808a0e059401ab4ac3c8436a5abccb5671bf3c4
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-02-09 19:07:30 +0100

    Add files via upload

---
+++ zadatak2TextBoxKontrole/zadatak2TextBoxKontrole/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadatak2TextBoxKontrole
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string var;
+            var = textBox1.Text;
+            string var1;
+            var1 = textBox2.Text;
+            MessageBox.Show(""+var + var1);
+        }
+    }
+}
